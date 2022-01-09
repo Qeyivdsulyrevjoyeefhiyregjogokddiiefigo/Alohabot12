23e4a6355800a2ede7a6b6588ed1c8d9d47f3ec6
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2022-01-09 10:03:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ fthumb = setting.fakethumb
 hit_today = []
 blocked = []
 ban = []
-limitawal = "50"
+limitawal = "9999999"
 gcounttprem = "55" 
 gcounttuser = "100" 
 
@@ -194,8 +194,8 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
+baper = 'Sabar Ya Om Orang Sabar Di Sayang Janda'
+apiku = 'wa.me/6288217843188'
 gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
